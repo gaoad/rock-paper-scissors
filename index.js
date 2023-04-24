3ef9519f4812d5d9ae125cbbd7ae1f39f8641437
April Gao <67171198+gaoad@users.noreply.github.com>
2023-04-23 23:44:20 -0400

    Implement correct win/lose rules and keep score

--- index.js
+++ index.js
@@ -1,24 +1,81 @@
 let choices = ["rock", "paper", "scissors"];
 let computerChoice;
-let playerChoice = prompt("Do you choose Rock, Paper, or Scissors?")
-let computerScore;
-let playerScore;
+let playerChoice;
+let computerScore = 0;
+let playerScore = 0;
 
 function getComputerChoice() {
     computerChoice = choices[Math.floor(Math.random()*choices.length)];
-    console.log("Computer chose " + computerChoice);
     return(computerChoice);
 }
 
-function playGame(playerChoice, computerChoice) {
-    
-    if (computerChoice==="rock" && playerChoice.toUpperCase()==="SCISSORS") {
-        console.log("You win!");
-    } else {
-        console.log("You lose");
-    }
+function win() {
+    console.log("You win!");
+    playerScore++;
 }
 
+function tie() {
+    console.log("Tie");    
+}
+
+function lose() {
+    console.log("You lose :'(");
+    computerScore++;
+}
+
+
+function playGame(playerChoice, computerChoice) {
+
+    playerChoice = prompt("Do you choose Rock, Paper, or Scissors?")
+    console.log("You chose " + playerChoice);
+    console.log("Computer chose " + computerChoice);
+
+    if (computerChoice==="rock") {
+        if (playerChoice.toUpperCase()==="SCISSORS") {
+            win();
+        } else if (playerChoice.toUpperCase()==="ROCK") {
+            tie();
+        } else if (playerChoice.toUpperCase()==="PAPER") {
+            lose();
+        } else {
+            console.log("Please enter a valid choice")
+        }
+    } else if (computerChoice==="paper") {
+        if (playerChoice.toUpperCase()==="SCISSORS") {
+            win();
+        } else if (playerChoice.toUpperCase()==="ROCK") {
+            lose();
+        } else if (playerChoice.toUpperCase()==="PAPER") {
+            tie();
+        } else {
+            console.log("Please enter a valid choice")
+        }
+    } else if (computerChoice==="scissors") {
+        if (playerChoice.toUpperCase()==="SCISSORS") {
+            tie();
+        } else if (playerChoice.toUpperCase()==="ROCK") {
+            win();
+        } else if (playerChoice.toUpperCase()==="PAPER") {
+            lose();
+        } else {
+            console.log("Please enter a valid choice")
+        }
+    } else {
+        console.log("Something went horribly wrong")
+    }
+
+    console.log("SCORE  You: " + playerScore + "    Computer: " + computerScore)
+
+}
+
+getComputerChoice();
+playGame(playerChoice, computerChoice);
+getComputerChoice();
+playGame(playerChoice, computerChoice);
+getComputerChoice();
+playGame(playerChoice, computerChoice);
+getComputerChoice();
+playGame(playerChoice, computerChoice);
 getComputerChoice();
 playGame(playerChoice, computerChoice);
 
@@ -26,7 +83,7 @@ playGame(playerChoice, computerChoice);
 
 /*
 Welcome screen
-Computer chooses random RPS
+ーComputer chooses random RPS
 Person clicks button to choose RPS
 Compare the two answers
 Determine winner based on RPS rules of order
